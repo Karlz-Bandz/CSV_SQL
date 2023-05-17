18901e6351a985375ab49787ef64d8f037b255d9
Melak <km250542@ncr.com>
2023-05-17 19:34:08 +0200

    conversion done

--- src/main/java/interfaces/Reader.java
+++ src/main/java/interfaces/Reader.java
@@ -1,11 +1,17 @@
 package interfaces;
 
+import com.opencsv.exceptions.CsvException;
+import models.Person;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public interface Reader
 {
     void displayFileReader() throws IOException;
 
     void readCsvFile(String path);
+
+    List<Person> convertCsv() throws IOException, CsvException;
 }
--- src/main/java/services/CsvReader.java
+++ src/main/java/services/CsvReader.java
@@ -1,12 +1,17 @@
 package services;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import interfaces.Reader;
 import lombok.NoArgsConstructor;
+import models.Person;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @NoArgsConstructor
@@ -14,20 +19,16 @@ public class CsvReader implements Reader
 {
     private FileReader fileReader;
 
-
     @Override
     public void displayFileReader() throws IOException
     {
         if(this.fileReader != null)
         {
             BufferedReader bufferedReader = new BufferedReader(this.fileReader);
-
-
             String line;
             while ((line = bufferedReader.readLine()) != null){
                 System.out.println(line);
             }
-
         }else{
             System.out.println("FileReader object is null");
         }
@@ -42,5 +43,27 @@ public class CsvReader implements Reader
         {
             System.out.println("File not found!");
         }
+    }
+
+    @Override
+    public List<Person> convertCsv() throws IOException, CsvException
+    {
+        List<Person> persons = new ArrayList<>();
+
+        if(this.fileReader != null)
+        {
+            CSVReader csvReader = new CSVReader(this.fileReader);
+            List<String[]> rows = csvReader.readAll();
+
+            for(int i = 1; i < rows.size(); i++)
+            {
+                int id = Integer.parseInt(rows.get(i)[0]);
+                Person person = new Person(id, rows.get(i)[1], rows.get(i)[2]);
+                persons.add(person);
+            }
+        }else{
+            System.out.println("File not found in convertCsv method");
+        }
+        return persons;
     }
 }
